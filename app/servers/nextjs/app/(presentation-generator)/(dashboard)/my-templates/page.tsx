7f0057409efc0d91936937d02b880057d27a497e
Monir <mr.monirbd@gmail.com>
2026-05-08 02:58:47 +0600

    updated

---
+++ app/servers/nextjs/app/(presentation-generator)/(dashboard)/my-templates/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TemplatePanel from "../templates/components/TemplatePanel";
+
+const page = () => {
+    return (
+        <TemplatePanel layout="user-grid" />
+    );
+};
+
+export default page;
