8acdc786d5a6c4896801922909dd86e96a2a8d65
Monir <mr.monirbd@gmail.com>
2026-06-18 16:26:45 +0600

    updated

--- app/servers/nextjs/app/layout.tsx
+++ app/servers/nextjs/app/layout.tsx
@@ -52,16 +52,16 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <Script id="google-tag-manager" strategy="beforeInteractive">
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      <body
+        className={`${inter.variable} ${unbounded.variable} ${syne.variable} antialiased`}
+      >
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-PCN89WT8');`}
-      </Script>
-      <body
-        className={`${inter.variable} ${unbounded.variable} ${syne.variable} antialiased`}
-      >
+        </Script>
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-PCN89WT8"
