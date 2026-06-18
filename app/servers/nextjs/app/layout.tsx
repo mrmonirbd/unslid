211e0d21572b06d4735c75fec81d7e6e944e9d86
Monir <mr.monirbd@gmail.com>
2026-06-18 12:00:48 +0600

    updated

--- app/servers/nextjs/app/layout.tsx
+++ app/servers/nextjs/app/layout.tsx
@@ -7,6 +7,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { CookieConsent } from "@/components/CookieConsent";
 import { GoogleAnalytics } from "@/components/GoogleAnalytics";
 import { Suspense } from "react";
+import Script from "next/script";
 
 const inter = localFont({
   src: [
@@ -51,9 +52,24 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <Script id="google-tag-manager" strategy="beforeInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PCN89WT8');`}
+      </Script>
       <body
         className={`${inter.variable} ${unbounded.variable} ${syne.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PCN89WT8"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Suspense>
           <GoogleAnalytics />
         </Suspense>
